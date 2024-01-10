73e78560abb6e7bfbd4795ba973699a51c8a0bb9
picuu <carloscapoforcada@gmail.com>
2024-01-10 12:08:37 +0100

    add logic to main() function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,31 @@
 fun main() {
     println("$WHITE_BACKGROUND_BRIGHT$BLACK_BOLD    CALCULATE WATER INVOICE    $RESET")
     val waterConsumption:Int = getWaterConsumption()
-    val isNumerous:Boolean = isNumerousFamily()
-    val isSingleParent:Boolean = isSingleParentFamily()
-    val familySize:Int = getFamilySize()
+
     val hasSocialBonus:Boolean = hasSocialBonus()
+    var isNumerous:Boolean = false
+    var isSingleParent:Boolean = false
+    var familySize:Int = 0
+
+    if (!hasSocialBonus) {
+        isNumerous = isNumerousFamily()
+        if (!isNumerous) {
+            isSingleParent = isSingleParentFamily()
+            if (isSingleParent) familySize = getFamilySize()
+        }
+    }
+
 
     val fixedFee:Int = getFixedFee(hasSocialBonus)
     val literCost:Float = getLiterCost(waterConsumption)
-    val waterCost:Float = getWaterCost(waterConsumption, literCost, isNumerous, isSingleParent, familySize, hasSocialBonus)
+    val waterCostOUT:Float = getWaterCost(waterConsumption, literCost, isNumerous, isSingleParent, familySize, hasSocialBonus)
+
+    var totalCost:Float = fixedFee.toFloat()
+
+    if (waterConsumption > 50) totalCost = fixedFee + waterCostOUT
+
+    println("$totalCost€")
+    // println(showTicket())
 }
 
 /**
