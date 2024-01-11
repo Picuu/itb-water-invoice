ca7e8fcd10d941353ea7375de52b0cfaf5578cd7
picuu <carloscapoforcada@gmail.com>
2024-01-11 21:11:00 +0100

    add logic to main() function to end program if water consumption is below 50 liters

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,27 +2,31 @@ fun main() {
     println("$WHITE_BACKGROUND_BRIGHT$BLACK_BOLD    CALCULATE WATER INVOICE    $RESET")
     val waterConsumption:Int = getWaterConsumption()
 
-    val hasSocialBonus:Boolean = hasSocialBonus()
+    var hasSocialBonus:Boolean = false
     var isNumerous:Boolean = false
     var isSingleParent:Boolean = false
     var familySize:Int = 0
-
-    if (!hasSocialBonus) {
-        isNumerous = isNumerousFamily()
-        if (!isNumerous) {
-            isSingleParent = isSingleParentFamily()
-            if (isSingleParent) familySize = getFamilySize()
-        }
-    }
-
-
-    val fixedFee:Int = getFixedFee(hasSocialBonus)
-    val literCost:Float = getLiterCost(waterConsumption)
-    val waterCostOUT:Float = getWaterCost(waterConsumption, literCost, isNumerous, isSingleParent, familySize, hasSocialBonus)
-
+    var fixedFee:Int = getFixedFee(false)
     var totalCost:Float = fixedFee.toFloat()
 
-    if (waterConsumption > 50) totalCost = fixedFee + waterCostOUT
+    if (waterConsumption >= 50) {
+        hasSocialBonus = hasSocialBonus()
+
+        if (!hasSocialBonus) {
+            isNumerous = isNumerousFamily()
+            if (!isNumerous) {
+                isSingleParent = isSingleParentFamily()
+                if (isSingleParent) familySize = getFamilySize()
+            }
+        }
+
+        fixedFee = getFixedFee(hasSocialBonus)
+        val literCost:Float = getLiterCost(waterConsumption)
+        val discount: Float = getDiscount(isNumerous, isSingleParent, familySize, hasSocialBonus)
+        val waterCost:Float = getWaterCost(waterConsumption, literCost, discount)
+
+        totalCost = fixedFee + waterCost
+    }
 
     println("$totalCost€")
     // println(showTicket())
